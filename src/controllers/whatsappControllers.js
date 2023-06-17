7b56baf0d898a39bab08d63ef7dbec3a5746cb87
aditaman <33344175+aditaman@users.noreply.github.com>
2023-06-17 22:01:46 +0700

    mengubah whatsappController.js

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,9 +1,36 @@
+const fs = require("fs");
+const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
+
 const VerifyToken = (req, res) => {
-  res.send("verify token");
+  try {
+    var accessToken = "RTQWWTVHBDEJHJKIKIKNDS9090DS";
+    var token = req.query["hub.verify_token"];
+    var challenge = req.query["hub.challenge"];
+
+    if (challenge != null && token != null && token == accessToken) {
+      res.send(challenge);
+    } else {
+      res.status(400).send();
+    }
+  } catch (e) {
+    res.status(400).send();
+  }
 };
 
 const receivedMessage = (req, res) => {
-  res.send("received message");
+  try {
+    var entry = req.body["entry"][0];
+    var changes = entry["changes"][0];
+    var value = changes["value"];
+    var messageObject = value["messages"];
+
+    myConsole.log(messageObject);
+
+    res.send("EVENT_RECEIVED");
+  } catch (e) {
+    myConsole.log(e);
+    res.send("EVENT_RECEIVED");
+  }
 };
 
 module.exports = {
